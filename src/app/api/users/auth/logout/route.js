e0bcb01487f40eb565977a49961e13bdd35103ff
alok verma <alokverma749@gmail.com>
2023-08-16 10:18:06 +0530

    added server component for logout

--- src/app/api/users/auth/logout/route.js
+++ src/app/api/users/auth/logout/route.js
@@ -1,12 +1,14 @@
+'use server'
 import { NextResponse } from "next/server";
+import { cookies } from 'next/headers'
 
 export async function GET() {
     try {
+        cookies().set('showcaseToken', '', { expires: Date.now() })
         const response = NextResponse.json({
             message: "Logout successfull",
             success: true
         }, { status: 200 })
-        response.cookies.delete("showcaseToken");
         return response;
     } catch (error) {
         return NextResponse.json({
